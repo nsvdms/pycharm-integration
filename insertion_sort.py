469fac3929ec396d30d433c5db955d9d5ab1c203
nsvdms <ksupraja96@gmail.com>
2021-09-14 16:32:15 +0530

    insertion sort committed

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,11 +1,11 @@
-def insertion_sort(inputList):
-    for i in range(1, len(inputList)):
+def insertion_sort(inputlist1):
+    for i in range(1, len(inputlist1)):
         j = i-1
-        nxt_element = inputList[i]
-        while (inputList[j] > nxt_element) and (j >= 0):
-            inputList[j + 1] = inputList[j]
+        nxt_element = inputlist1[i]
+        while (inputlist1[j] > nxt_element) and (j >= 0):
+            inputlist1[j + 1] = inputlist1[j]
             j = j-1
-        inputList[j + 1] = nxt_element
+        inputlist1[j + 1] = nxt_element
 
 
 lists = [19, 2, 31, 45, 30, 11, 121, 27]
